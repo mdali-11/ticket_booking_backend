8db4aa299aaec22b17a87be176b1358ae52aee89
unknown <mdali91935@gmail.com>
2023-04-25 16:53:19 +0530

    API

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const {userRouter} = require("./routes/user.router")
 const cors=require("cors")
 require("dotenv").config();
 
+
 const app=express();
-app.use(cors)
 app.use(express.json())
+app.use(cors({
+    origin:"*"
+}))
+
 
 
 app.get("/",(req,res)=>{
     res.send("Welcome to my database")
     console.log("hello")
 })
-
-
-app.use("/users", userRouter);
+app.use("/users", userRouter)
 
 
 app.listen(process.env.PORT, async ()=>{
